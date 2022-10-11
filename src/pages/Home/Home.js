1795c8bd1a4b7a8f50695be703f0ab71a2c79f5b
Laura Aydelotte <Stella@lauras-air.attlocal.net>
2022-10-11 16:24:22 -0700

    fixed router issue

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,12 +1,12 @@
 import React from 'react'
-import Navbar from '../../components/Navbar'
+// import Navbar from '../../components/Navbar'
 import AboutCard from'../../components/AboutCard'
 
 const Home = () => {
   return(
     <>
-     <Navbar/>
-      <AboutCard />
+     {/* <Navbar/> */}
+     <AboutCard />
     </>
   )
 }
