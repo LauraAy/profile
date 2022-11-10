56738e844400cb46b2fa22efb722d9a47ccfba20
laura_ay <laura.ay1564@gmail.com>
2022-11-10 12:22:07 -0800

    added text

--- src/components/AboutCard/AboutCard.js
+++ src/components/AboutCard/AboutCard.js
@@ -8,18 +8,32 @@ import Card from 'react-bootstrap/Card';
 
 const AboutCard = () => {
  return ( 
-   <div class="blend_card" style={{width: "60rem"}} >
-      <Card.Body>
-        <Card.Title>Laura Aydelotte</Card.Title>
-        <Card.Subtitle className="mb-2 text-muted">Developer, Project Manager, Media Historian</Card.Subtitle>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </Card.Text>
-        <Card.Link href="#">Card Link</Card.Link>
-        <Card.Link href="#">Another Link</Card.Link>
-      </Card.Body>
+  <div class="container fluid">
+    <div class="row">
+      <div class="col">
+        <h2>Column</h2>
+      </div>
+      <div class="col">
+        <div class="container fluid" >
+        <div class="blend_card"  style={{ }}>
+          <Card.Body>
+            <Card.Title>Laura Aydelotte</Card.Title>
+            <Card.Subtitle className="mb-2 text-muted">Developer, Project Manager, Book and Media Historian</Card.Subtitle>
+            <Card.Text>
+            I'm a full stack developer dedicated to creating user centered applications with an elegant blend form and function. My coding skills grew out of my experience as a project manager guiding projects through the lifecycle from start to completion. My understanding of both technical practice and larger level project needs makes me adept at both building projects with creative solutions in response to user needs and fostering communication and collaboration between the user and development sides. 
+            <br></br>
+            <br></br>
+            With a Master's in Library and Information Science, and a PhD specializing in book and media history, I offer a particular blend of expertise that knows how to work with both big data and big ideas. This background means I offer a unique blend of specialized knowledge to libraries, universities and other parts of the cultural heritage sector, and I bring a fresh creative perspective and innovative problem solving approach to a variety of other industries. 
+
+            </Card.Text>
+            <Card.Link href="#">Card Link</Card.Link>
+            <Card.Link href="#">Another Link</Card.Link>
+          </Card.Body>
+         </div> 
+         </div>
+      </div>
     </div>
+  </div>
  
   )
 }
