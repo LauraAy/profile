70f976944635d9c07ec725e4fb50de4370d14737
laura_ay <laura.ay1564@gmail.com>
2022-11-30 16:37:48 -0800

    text for three columns added

--- src/components/ThreeLinkCards/ThreeLinkCards.js
+++ src/components/ThreeLinkCards/ThreeLinkCards.js
@@ -1,36 +1,65 @@
 import React from 'react'
-// import {LinkContainer} from 'react-router-bootstrap'
+import {LinkContainer} from 'react-router-bootstrap'
 
 const ThreeLinkCards = () => {
     return ( 
 
 <div class="container">
-  <div class="row" width="100%">
-    
+    <div class="row" width="100%"> 
     <div class="col-sm blend_card">
-     <h4>Developer</h4>
-     <hr></hr>
-     <p>Interested in the code? Click here to see a sample of my development work.
+        <LinkContainer to="/developer" role="button">
+        <h4>Developer</h4>
+        </LinkContainer>  
+        <hr></hr>
+        <LinkContainer to="/developer" role ="button">
+        <p>
+        Interested in the code? Click here to see a sample of my development work.
+        </p>
+        </LinkContainer> 
         <ul>
             <li>Javascript & React</li>
             <li>Ruby on Rails</li>
             <li>SQL</li>
+            <li>Data Management</li>
             <li>and more...</li>
         </ul>  
-     </p>
     </div>
 
     <div class="col-sm blend_card">
-     <h4>Project Manager</h4>
-     <hr></hr>
+    <LinkContainer to="/manager" role="button">
+        <h4>Project Manager</h4>
+        </LinkContainer>  
+        <hr></hr>
+        <LinkContainer to="/manager" role ="button">
+        <p>
+        Looking for a project manager? Click here to learn more about my management experience.
+        </p>
+        </LinkContainer> 
+        <ul>
+            <li>Agile and Scrum</li>
+            <li>Grant Writing Experience</li>
+            <li>and more...</li>
+        </ul>  
     </div>
 
     <div class="col-sm blend_card">
-      <h4>Curator & Historian</h4>
-      <hr></hr>
+    <LinkContainer to="/historian" role="button">
+        <h4>Curator & Historian</h4>
+        </LinkContainer>  
+        <hr></hr>
+        <LinkContainer to="/historian" role ="button">
+        <p>
+        Click here to learn more about my experience as a curator and historian of book and media history.
+        </p>
+        </LinkContainer> 
+        <ul>
+            <li>Special Collections Curator</li>
+            <li>Early Modern Scholar</li>
+            <li>Provenance Expert</li>
+            <li>and more...</li>
+        </ul>  
     </div>
-   
-  </div>
+    </div>
 </div>
 
  )
