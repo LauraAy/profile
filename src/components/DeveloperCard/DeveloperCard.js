398f7ffb08b18ceaf15bd03ed6a46fe3a7254c2f
laura_ay <laura.ay1564@gmail.com>
2022-12-01 17:35:51 -0800

    started development page

--- src/components/DeveloperCard/DeveloperCard.js
+++ src/components/DeveloperCard/DeveloperCard.js
@@ -1,14 +1,46 @@
 import React from 'react'
 import Card from 'react-bootstrap/Card';
 import Container from 'react-bootstrap/Container';
+import SkillList from '../../components/SkillList';
+// import DataSkills from '../../components/DataSkills';
 
 const DeveloperCard = () => {
     return (
-        <Container>
-            <Card>
-                <h2>Development Stuff Here</h2>
-            </Card>
-        </Container>
+<Container>
+    <div class="blend_card">
+        <Card.Body>
+            <Card.Title>
+            Development and Data Work
+            </Card.Title>
+            I offer fullstack application development, specializing in Javascript and React, with additional experience with Python and Ruby on Rails. Familiar with both SQL and mongoDB. Also experienced in producing and curating data and metadata. 
+            <br></br>
+            <br></br>
+            <h4>Skills</h4>
+                <SkillList />
+            {/* <h4>Data Curation Skills</h4>
+                <DataSkills /> */}
+
+
+                {/* <ul>
+                    <li style="display:inline">JavaScript</li>
+                    <li style="display:inline">React</li>
+                    <li style="display:inline">Python</li>
+                    <li style="display:inline">Ruby on Rails</li>
+                    <li style="display:inline">HTML</li>
+                    <li style="display:inline">JXS</li>
+                    <li style="display:inline">CSS</li>
+                    <li style="display:inline">SASS</li>
+                    <li style="display:inline">SQL</li>
+                    <li style="display:inline">mongoDB</li>
+                </ul> */}
+
+            <Card.Text>
+
+
+            </Card.Text>
+        </Card.Body>
+    </div>
+</Container>
 
     )
 }
---
+++ src/components/SkillList/SkillList.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+function SkillList() {
+  return (
+    <> 
+    <div class="row" width="100%"> 
+     <div class="col">
+        <ul>
+          <li>JavaScript</li>
+          <li>React</li>
+          <li>Python</li>
+          <li>Ruby on Rails</li>
+        </ul>
+      </div>
+      <div class="col">
+        <ul>
+          <li>HTML</li>
+          <li>JXS</li>
+          <li>CSS</li>
+          <li>SASS</li>
+        </ul>
+      </div>
+      <div class="col">
+        <ul>
+          <li>SQL</li>
+          <li>mongoDB</li>
+        </ul>
+      </div>
+    </div> 
+    </>
+  )
+};
+
+export default SkillList;
