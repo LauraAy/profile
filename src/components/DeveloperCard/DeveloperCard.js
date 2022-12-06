7df6f78f6b42b6708a1f6b465715dd19bd82825f
laura_ay <laura.ay1564@gmail.com>
2022-12-06 14:38:25 -0800

    Dev Page done

--- src/components/DeveloperCard/DeveloperCard.js
+++ src/components/DeveloperCard/DeveloperCard.js
@@ -9,9 +9,9 @@ const DeveloperCard = () => {
 <Container>
     <div class="blend_card">
         <Card.Body>
-            <Card.Title>
+            <h3>
             Development and Data Work
-            </Card.Title>
+            </h3>
             <Card.Text>
             I offer fullstack application development, specializing in Javascript and React, with additional experience with Python and Ruby on Rails. Familiar with both SQL and mongoDB. I also have a Master's in Library and Information Science with a speciality in data curation and experience creating and maintaining data and metadata. 
             <br></br>
