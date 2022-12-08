df73e3d011515b9b153dbb92408cd0bee0ad2f8e
laura_ay <laura.ay1564@gmail.com>
2022-12-07 17:14:44 -0800

    text for historian card added

--- src/components/HistorianCard/HistorianCard.js
+++ src/components/HistorianCard/HistorianCard.js
@@ -4,11 +4,39 @@ import Container from 'react-bootstrap/Container';
 
 const HistorianCard = () => {
     return (
-        <Container>
-            <Card>
-                <h2>Historian Stuff Here</h2>
-            </Card>
-        </Container>
+<Container>
+    <div class="blend_card">
+        <Card.Body>
+            <h3>
+            Curator and Historian
+            </h3>
+            <Card.Text>
+            I come to the world of technology from a background as an historian of Books and Media.
+            My scholarly interests range from the era when the manuscript culture of the European middle ages 
+            transitioned into the start of printing with the 1450 invention of the printing press, to our own 
+            modern era when that era of print technology is transitioning into the internet age. 
+            <br></br>
+            <br></br>
+            I hold a PhD from the University of Chicago specializing in Early Modern European Literature and Culture, 
+            and my dissertation project on the description and representation of architecture in plays and poetry from
+            Chaucer to Shakespeare reflected my early interest in interdisciplinary work studying the history of how 
+            word and image interacted in early books and manuscripts. 
+            <br></br>
+            <br></br>
+            For my Master's in Library and Information Science at the University of Illinois Urbaba-Champaign, I did a dual 
+            specialization in special collections librarianship, and data curation. This led to my work as both a 
+            curator working to maintain, preserve, and grow collections of historical books and manuscripts, 
+            and as a digital specialist supervising digital projects that provide creative technical solutions
+            to support the description and interpretation of those historical materials, as well as making images of 
+            special collections openly available online to share with the world. My role as curator of the Furness Shakespeare Libary at 
+            Penn Libraries led to my work on the Philadelphia Playbills Project. My three years of work on the Provenance 
+            Online Project (POP), which aimed to digitally crowdsource identifications of ownership marks in rare books, also
+            provided me with the experience of working with a wide range of physical collection materials (largely American and European), 
+            from the middle ages to the present day in collections from around the country. 
+             </Card.Text>
+        </Card.Body>
+    </div>
+</Container>
 
     )
 }
