4973d42c4c999e3123a30f4c6b0a310f03ad9a7d
laura_ay <laura.ay1564@gmail.com>
2024-01-23 14:54:12 -0800

    change url

--- src/components/DevPortfolio/DevPortfolio.js
+++ src/components/DevPortfolio/DevPortfolio.js
@@ -17,7 +17,7 @@ const DevPortfolio = () => {
         <Row xs={1} md={2} className="blend_card2">
             <Col>
                 <Container className="blend_card2">
-                    <a href='http://www.yourworldcookbook.com'>
+                    <a href='https://www.yourworldcookbook.com'>
                     <Card.Img variant="top" src={Recipe} style={{maxHeight: "220px"}}/>
                     </a>
                     <card border='light'>
@@ -27,7 +27,7 @@ const DevPortfolio = () => {
                         <Card.Text>
                         A fullstack javascript application for entering recipes and attaching information about the region the recipe is from and more. React frontend and a backend built with a MySQL database using node.js, express, and sequelize.  
                         </Card.Text>
-                        <a href="http://www.yourworldcookbook.com" class='btn btn-primary m-2'>Website</a>
+                        <a href="https://www.yourworldcookbook.com" class='btn btn-primary m-2'>Website</a>
                         <a href = "https://github.com/LauraAy/Your-World-Cookbook" class='btn btn-primary'>Github</a>
                     </Card.Body>
                     </card>
