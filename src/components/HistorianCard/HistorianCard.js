43cb10d2b39cda53bff7623266ed5e3f716b1447
laura_ay <laura.ay1564@gmail.com>
2025-06-23 14:26:10 -0700

    new historian card text

--- src/components/HistorianCard/HistorianCard.js
+++ src/components/HistorianCard/HistorianCard.js
@@ -13,41 +13,13 @@ const HistorianCard = () => {
             Curator and Historian
             </h3>
             <Card.Text>
-            I come to the world of technology from a background as an historian of Books and Media.
-            My scholarly interests range from the era when the manuscript culture of the European middle ages 
-            transitioned into the start of printing with the 1450 invention of the printing press, to our own 
-            modern era when that era of print technology is transitioning into the internet age. I have over 6 years
-            of experience working with Special Collections materials at the Newberry Library, Princeton University,
-            and the University of Pennsylvania, including my role as interim curator for the Furness Shakespeare Library at Penn
-            Libraries. 
+            As both a librarian and a historian of books and media, I am passionate about the work of describing, maintaining, and sharing knowledge about historical books, manuscripts, and other primary resources.  I hold a Master's in Library and Information Science from the University of Illinois Urbana-Champaign, with a dual specialization in special collections librarianship and data curation, and a PhD from the University of Chicago specializing in Early Modern European Literature and Culture. I have over 6 years of experience working with Special Collections materials at the Newberry Library, Princeton University, and the University of Pennsylvania. In these roles I worked in both traditional library and archival roles to describe, provide access to, and teach in-person sessions with library materials, and as a data curation specialist supervising digital projects that provide creative technical solutions to support the description and interpretation of historical materials and make images and information about our history available to the world. 
             <br></br>
             <br></br>
-            I hold a PhD from the University of Chicago specializing in Early Modern European Literature and Culture, 
-            and my dissertation project on the description and representation of architecture in plays and poetry from
-            Chaucer to Shakespeare reflected my early interest in interdisciplinary work studying the history of how 
-            word and image interact in early books and manuscripts. 
+            In addition to my curatorial work, I’ve led two major digital projects focused on the description of special collections and archives materials.  My role as interim curator of the Furness Shakespeare Library at Penn Libraries led to me writing the successful NEH grant that launched the Philadelphia Playbills Project to crowdsource transcriptions of and produce quality metadata for Penn's collection of theatrical playbills. My three years of work on the Provenance Online Project (POP), which digitally crowdsourced identifications of ownership marks in rare books, provided me with the experience of working closely with a wide range of physical collection materials (largely American and European), from the middle-ages to the present day in collections from around the country. My work on POP included collaboration with libraries both nationally and internationally to establish metadata standards for describing ownership provenance in special collections and archives.
             <br></br>
             <br></br>
-            For my Master's in Library and Information Science at the University of Illinois Urbaba-Champaign, I did a dual 
-            specialization in special collections librarianship and data curation. This led to my work as both a 
-            curator working to maintain, preserve, and grow collections of historical books and manuscripts, 
-            and as a digital specialist supervising digital projects that provide creative technical solutions
-            to support the description and interpretation of those historical materials, as well as making images of 
-            special collections material openly available online to share with the world. 
-            <br></br>
-            <br></br>
-            My role as interim curator of the Furness Shakespeare Libary at 
-            Penn Libraries led to me writing the successful NEH grant that launched the Philadelphia Playbills Project to crowdsource transcriptions
-            and produce quality metadata for Penn's collectoin of theatrical playbills. My three years of work on the Provenance 
-            Online Project (POP), which aimed to digitally crowdsource identifications of ownership marks in rare books, meant 
-            provided me with the experience of working closely with a wide range of physical collection materials (largely American and European), 
-            from the middle ages to the present day in collections from around the country. My work on POP included collaboration with 
-            libraries both nationally and internationally to establish metadata standards for describing ownership provenance in special 
-            collections and archives.
-            <br></br>
-            <br></br>
-            Some of my current research activities include a book in progress on the history of women's book ownership pre 1800, and
-            a digital humanities project to map all the locations mentioned in Shakespeare's plays. 
+            As a historian of books and media, I’ve been fascinated by the way information is created and expressed from my doctoral research, which focused on the role of word and image in the transitional period from manuscript to print across the late middle ages into the Renaissance, to my library work, which has focused on the way the use of images of primary sources combined text in the form of metadata and code shape how we learn on the internet today. Other current research activities include a book in progress on the history of women's book ownership and provenance marks pre-1800, and a digital humanities project to map all the locations mentioned in Shakespeare's plays.
              </Card.Text>
         </Card.Body>
     </div>
